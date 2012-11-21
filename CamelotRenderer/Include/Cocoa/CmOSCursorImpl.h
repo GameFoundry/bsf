58db262ed79822431940853ed393d584d6c5f0c4
Marko Pintera <bearishsun@gmail.com>
2012-11-21 19:10:21 +0000

    Linux/Mac OSCursor implementations (untested)

---
+++ CamelotRenderer/Include/Cocoa/CmOSCursorImpl.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CmPrerequisitesUtil.h"
+#include "CmInt2.h"
+
+#if defined(__OBJC__)
+#import <Cocoa/Cocoa.h>
+#else
+#include <ApplicationServices/ApplicationServices.h>
+typedef void *id;
+#endif
+
+namespace CamelotEngine
+{
+	/**
+	 * @brief	Provides controls for Windows operating system cursor.
+	 */
+	class CM_UTILITY_EXPORT OSCursor
+	{
+	public:
+		static Int2 getPosition();
+		static void setPosition(const Int2& pos);
+
+		static void hide();
+		static void show();
+	};
+}
---
+++ CamelotRenderer/Include/X11/CmOSCursorImpl.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CmPrerequisitesUtil.h"
+#include "CmInt2.h"
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+namespace CamelotEngine
+{
+	/**
+	 * @brief	Provides controls for operating system cursor.
+	 */
+	class CM_UTILITY_EXPORT OSCursor
+	{
+	public:
+		OSCursor();
+
+		static Int2 getPosition();
+		static void setPosition(const Int2& pos);
+
+		static void hide();
+		static void show();
+
+	private:
+		Window mWindow; // TODO - This isn't being set anywhere yet
+		Cursor mCursor;
+		Display* mDisplay;
+
+		bool mCursorGrabbed;
+		bool mCursorHidden;
+	};
+}
