2fb6cdf494a55776164230e9528ec10f8697a050
Marko Pintera <bearishsun@gmail.com>
2013-02-21 21:39:52 +0000

    Updated CamelotEditor with fixed Qt binary plugins

--- CamelotEditor/Source/main.cpp
+++ CamelotEditor/Source/main.cpp
@@ -1,11 +1,22 @@
 #include "CmQtEditor.h"
 #include <string>
 #include <QtWidgets/QApplication>
+#include <windows.h>
 
-int main(int argc, char *argv[])
+int CALLBACK WinMain(
+	_In_  HINSTANCE hInstance,
+	_In_  HINSTANCE hPrevInstance,
+	_In_  LPSTR lpCmdLine,
+	_In_  int nCmdShow
+	)
 {
+	int argc = 1;
+	char** argv = new char*[1];
+	argv[0] = "CamelotEditor.exe";
+
 	QApplication a(argc, argv);
 	CamelotEditor w;
 	w.show();
 	return a.exec();
 }
+
