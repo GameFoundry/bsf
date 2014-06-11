25181b993e751c188ebc5b04b334b873c76b781b
BearishSun <bearishsun@gmail.com>
2014-06-11 13:46:33 +0200

    More documentation

--- BansheeEngine/Include/BsGUIElementContainer.h
+++ BansheeEngine/Include/BsGUIElementContainer.h
@@ -5,6 +5,10 @@
 
 namespace BansheeEngine
 {
+	/**
+	 * @brief	Helper GUI element class that is meant for hold other GUI elements.
+	 *			(Similar as GUILayout, but you may position elements manually).
+	 */
 	class BS_EXPORT GUIElementContainer : public GUIElement
 	{
 	protected:
@@ -12,31 +16,34 @@ namespace BansheeEngine
 		virtual ~GUIElementContainer();
 
 		/**
-		 * @copydoc GUIElement::getNumRenderElements()
+		 * @copydoc GUIElement::getNumRenderElements
 		 */
 		virtual UINT32 getNumRenderElements() const;
 
 		/**
-		 * @copydoc GUIElement::getMaterial()
+		 * @copydoc GUIElement::getMaterial
 		 */
 		virtual const GUIMaterialInfo& getMaterial(UINT32 renderElementIdx) const;
 
 		/**
-		 * @copydoc GUIElement::getNumQuads()
+		 * @copydoc GUIElement::getNumQuads
 		 */
 		virtual UINT32 getNumQuads(UINT32 renderElementIdx) const;
 
 		/**
-		 * @copydoc GUIElement::fillBuffer()
+		 * @copydoc GUIElement::fillBuffer
 		 */
 		virtual void fillBuffer(UINT8* vertices, UINT8* uv, UINT32* indices, UINT32 startingQuad, 
 			UINT32 maxNumQuads, UINT32 vertexStride, UINT32 indexStride, UINT32 renderElementIdx) const;
 
 		/**
-		 * @copydoc GUIElement::updateBounds()
+		 * @copydoc GUIElement::updateClippedBounds
 		 */
 		virtual void updateClippedBounds();
 
+		/**
+		 * @copydoc GUIElement::_getOptimalSize
+		 */
 		virtual Vector2I _getOptimalSize() const;
 	};
 }
