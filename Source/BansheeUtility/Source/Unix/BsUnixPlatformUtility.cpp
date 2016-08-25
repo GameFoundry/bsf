185fe79c2b205b5812eaa16e10d7d91bc20ac3ed
Marc Legendre <marc.legendre@ymail.com>
2016-08-25 03:00:44 +0200

    Implement PlatformUtility::terminate() on Linux
    Former-commit-id: 1264d6ace52525d132b28d53a6ad22c6f9397d18

---
+++ Source/BansheeUtility/Source/Unix/BsUnixPlatformUtility.cpp
@@ -0,0 +1,13 @@
+//********************************** Banshee Engine (www.banshee3d.com) **************************************************//
+//**************** Copyright (c) 2016 Marko Pintera (marko.pintera@gmail.com). All rights reserved. **********************//
+#include "BsPlatformUtility.h"
+
+#include <stdlib.h>
+
+namespace BansheeEngine
+{
+	void PlatformUtility::terminate(bool force)
+	{
+		exit(0);
+	}
+}
