faa85702fd856ddee7e94e7a09810768560e99e4
BearishSun <bearishsun@gmail.com>
2016-09-27 10:45:18 +0200

    Added a base undo/redo command class to scripting, so that custom undo/redo operations can be recorded
    Former-commit-id: f243fadd5e2a1dcce848ca6ff957f303e19f0824

---
+++ Source/MBansheeEditor/Utility/UndoableCommand.cs
@@ -0,0 +1,32 @@
+﻿//********************************** Banshee Engine (www.banshee3d.com) **************************************************//
+//**************** Copyright (c) 2016 Marko Pintera (marko.pintera@gmail.com). All rights reserved. **********************//
+using System;
+using System.Runtime.CompilerServices;
+using BansheeEngine;
+
+namespace BansheeEditor
+{
+    /// <summary>
+    /// Base class for implementation of commands that can be recorded in the UndoRedo system.
+    /// </summary>
+    public abstract class UndoableCommand : ScriptObject
+    {
+        public UndoableCommand()
+        {
+            Internal_CreateInstance(this);
+        }
+
+        /// <summary>
+        /// Applies the command, committing the change.
+        /// </summary>
+        protected abstract void Commit();
+
+        /// <summary>
+        /// Reverts the command, reverting the change previously done with <see cref="Commit"/>.
+        /// </summary>
+        protected abstract void Revert();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern void Internal_CreateInstance(UndoableCommand instance);
+    }
+}
---
+++ Source/SBansheeEditor/Include/BsManagedEditorCommand.h
@@ -0,0 +1,87 @@
+//********************************** Banshee Engine (www.banshee3d.com) **************************************************//
+//**************** Copyright (c) 2016 Marko Pintera (marko.pintera@gmail.com). All rights reserved. **********************//
+#pragma once
+
+#include "BsScriptEditorPrerequisites.h"
+#include "BsScriptObject.h"
+#include "BsEditorCommand.h"
+
+namespace BansheeEngine
+{
+	class CmdManaged;
+
+	/** @addtogroup ScriptInteropEditor
+	 *  @{
+	 */
+
+	/**	Interop class between C++ & CLR for CmdManaged. */
+	class BS_SCR_BED_EXPORT ScriptCmdManaged : public ScriptObject <ScriptCmdManaged>
+	{
+	public:
+		SCRIPT_OBJ(EDITOR_ASSEMBLY, "BansheeEditor", "UndoableCommand")
+
+		~ScriptCmdManaged();
+
+	private:
+		friend class CmdManaged;
+
+		ScriptCmdManaged(MonoObject* instance);
+
+		/** Triggers the Commit() method on the managed object instance. */
+		void triggerCommit();
+
+		/** Triggers the Revert() method on the managed object instance. */
+		void triggerRevert();
+
+		/** Notifies the script instance that the underlying managed command was destroyed. */
+		void notifyCommandDestroyed();
+
+		CmdManaged* mManagedCommand;
+
+		/************************************************************************/
+		/* 								CLR HOOKS						   		*/
+		/************************************************************************/
+		static void internal_CreateInstance(MonoObject* instance);
+
+		static MonoMethod* sCommitMethod;
+		static MonoMethod* sRevertMethod;
+	};
+
+	/** @} */
+
+	/** @addtogroup SBansheeEditor
+	 *  @{
+	 */
+
+	/** 
+	 * A command used for undo/redo purposes. This particular implementation provides a generic overridable base to be
+	 * implemented by script classes. 
+	 */
+	class BS_SCR_BED_EXPORT CmdManaged : public EditorCommand
+	{
+	public:
+		~CmdManaged();
+
+		/** @copydoc EditorCommand::commit */
+		void commit() override;
+
+		/** @copydoc EditorCommand::revert */
+		void revert() override;
+
+	private:
+		friend class UndoRedo;
+		friend class ScriptCmdManaged;
+
+		CmdManaged(ScriptCmdManaged* scriptObj);
+
+		/** 
+		 * Notifies the command the managed script object instance it is referencing has been destroyed. Normally when this
+		 * happens the command should already be outside of the undo/redo stack, but we clear the instance just in case.
+		 */
+		void notifyScriptInstanceDestroyed();
+
+		ScriptCmdManaged* mScriptObj;
+	};
+
+	/** @} */
+}
