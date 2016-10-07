906c53e0832bf351245a2c06741814d4151029d3
BearishSun <bearishsun@gmail.com>
2016-10-07 11:36:46 +0200

    Updating docs
    Former-commit-id: 2efdfb18006d63449e53a81eff843075ced708af

--- Source/BansheeCore/Include/BsCommandBuffer.h
+++ Source/BansheeCore/Include/BsCommandBuffer.h
@@ -20,7 +20,22 @@ namespace BansheeEngine
 	class BS_CORE_EXPORT CommandBuffer
 	{
 	public:
-		/** @copydoc CommandBufferManager::create */
+		/**
+		 * Creates a new CommandBuffer.
+		 * 
+		 * @param[in]	type		Determines what type of commands can be added to the command buffer.
+		 * @param[in]	deviceIdx	Index of the GPU the command buffer will be used to queue commands on. 0 is always
+		 *							the primary available GPU.
+		 * @param[in]	syncMask	Determines how is concurrency handled between multiple command buffers on the same 
+		 *							device. If buffers don't share the same bits in the sync mask they are allowed to
+		 *							execute concurrently on the GPU, however it is up to the user to ensure that they
+		 *							do not contain any resources depended on each other. Leave on default to ensure
+		 *							no concurrency is possible (safest).
+		 * @param[in]	secondary	If true the command buffer will not be allowed to execute on its own, but it can
+		 *							be appended to a primary command buffer. 
+		 * @return					New CommandBuffer instance.
+		 * 
+		 */
 		static SPtr<CommandBuffer> create(CommandBufferType type, UINT32 deviceIdx = 0, UINT32 syncMask = 0xFFFFFFFF, 
 			bool secondary = false);
 
