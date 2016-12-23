27b7b9eeccd1dba1ae5d101a1cbaf59fe5c5ab17
BearishSun <bearishsun@gmail.com>
2016-12-23 15:12:35 +0100

    Vulkan: Fixing issues caused by flipped viewport
    Former-commit-id: 10d51292ad31a9c4487747f6ed51d1c747c6f1c4

--- Source/BansheeVulkanRenderAPI/Source/BsVulkanRenderAPI.cpp
+++ Source/BansheeVulkanRenderAPI/Source/BsVulkanRenderAPI.cpp
@@ -548,7 +548,7 @@ namespace bs
 
 	const RenderAPIInfo& VulkanRenderAPI::getAPIInfo() const
 	{
-		static RenderAPIInfo info(0.0f, 0.0f, 0.0f, 1.0f, VET_COLOR_ABGR, false, true, true, true);
+		static RenderAPIInfo info(0.0f, 0.0f, 0.0f, 1.0f, VET_COLOR_ABGR, false, false, true, true);
 
 		return info;
 	}
