f893247c6bd389735819f4f7d8001537f5f643e7
BearishSun <bearishsun@gmail.com>
2018-01-16 17:28:18 +0100

    Bugfix: AudioSource component will now properly persist its properties
    Former-commit-id: d58647827bd5bbf11e268dbaf68992709aa54743

--- Source/BansheeCore/RTTI/BsCAudioSourceRTTI.h
+++ Source/BansheeCore/RTTI/BsCAudioSourceRTTI.h
@@ -17,8 +17,15 @@ namespace bs
 	class BS_CORE_EXPORT CAudioSourceRTTI : public RTTIType<CAudioSource, Component, CAudioSourceRTTI>
 	{
 		BS_BEGIN_RTTI_MEMBERS
-			BS_RTTI_MEMBER_REFLPTR(mInternal, 0)
+			//BS_RTTI_MEMBER_REFLPTR(mInternal, 0)
 			BS_RTTI_MEMBER_PLAIN(mPlayOnStart, 1)
+			BS_RTTI_MEMBER_REFL(mAudioClip, 2)
+			BS_RTTI_MEMBER_PLAIN(mVolume, 3)
+			BS_RTTI_MEMBER_PLAIN(mPitch, 4)
+			BS_RTTI_MEMBER_PLAIN(mLoop, 5)
+			BS_RTTI_MEMBER_PLAIN(mPriority, 6)
+			BS_RTTI_MEMBER_PLAIN(mMinDistance, 7)
+			BS_RTTI_MEMBER_PLAIN(mAttenuation, 8)
 		BS_END_RTTI_MEMBERS
 	public:
 		CAudioSourceRTTI()
