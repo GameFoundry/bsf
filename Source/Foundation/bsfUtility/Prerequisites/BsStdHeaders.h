6cfa88d918f4c0a1703a3b1bca599d8bc90c67c7
jonesmz <jonesmz@users.noreply.github.com>
2018-05-08 00:46:18 -0500

    Make EnumClassHash operator() constexpr
    It's just a static_cast, no reason it shouldn't be constexpr.
    
    By the way, I notice that there's no typedef for std::forward_list. It has slightly better performance than std::list for certain operations (and worse for others, of course).

--- Source/Foundation/bsfUtility/Prerequisites/BsStdHeaders.h
+++ Source/Foundation/bsfUtility/Prerequisites/BsStdHeaders.h
@@ -72,9 +72,11 @@ extern "C" {
 
 #if BS_PLATFORM == BS_PLATFORM_OSX
 extern "C" {
+
 #   include <unistd.h>
 #   include <sys/param.h>
 #   include <CoreFoundation/CoreFoundation.h>
+
 }
 #endif
 
@@ -93,7 +95,7 @@ namespace bs
 	struct EnumClassHash
 	{
 		template <typename T>
-		std::size_t operator()(T t) const
+		constexpr std::size_t operator()(T t) const
 		{
 			return static_cast<std::size_t>(t);
 		}
