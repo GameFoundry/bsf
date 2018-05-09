f4ae2289571b14b2e8da6bc350c9a7b531555a97
BearishSun <bearishsun@gmail.com>
2018-05-09 16:43:15 +0200

    Build: Generate separate debug symbol files on Linux & macOS  - Enabled debug symbols for all platforms in release mode

--- Source/CMake/HelperMethods.cmake
+++ Source/CMake/HelperMethods.cmake
@@ -319,3 +319,72 @@ function(check_and_update_builtin_assets ASSET_PREFIX ASSET_FOLDER ASSET_VERSION
 		endif()
 	endif()
 endfunction()
+
+function(strip_symbols targetName outputFilename)
+	if(UNIX)
+		if(CMAKE_BUILD_TYPE STREQUAL Release)
+			set(fileToStrip $<TARGET_FILE:${targetName}>)
+
+			# macOS
+			if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+				set(symbolsFile ${fileToStrip}.dwarf)
+
+				add_custom_command(
+					TARGET ${targetName}
+					POST_BUILD
+					VERBATIM 
+					COMMAND ${DSYMUTIL_TOOL} --flat --minimize ${fileToStrip}
+					COMMAND ${STRIP_TOOL} -u -r ${fileToStrip}
+					COMMENT Stripping symbols from ${fileToStrip} into file ${symbolsFile}
+				)
+			
+			# Linux
+			else()
+				set(symbolsFile ${fileToStrip}.dbg)
+
+				add_custom_command(
+					TARGET ${targetName}
+					POST_BUILD
+					VERBATIM 
+					COMMAND ${OBJCOPY_TOOL} --only-keep-debug ${fileToStrip} ${symbolsFile}
+					COMMAND ${OBJCOPY_TOOL} --strip-unneeded ${fileToStrip}
+					COMMAND ${OBJCOPY_TOOL} --add-gnu-debuglink=${symbolsFile} ${fileToStrip}
+					COMMENT Stripping symbols from ${fileToStrip} into file ${symbolsFile}
+				)
+			endif(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+
+			set(${outputFilename} ${symbolsFile} PARENT_SCOPE)
+		endif()
+	endif()
+endfunction()
+
+function(install_bsf_target targetName)
+	strip_symbols(${targetName} symbolsFile)
+	
+	install(
+		TARGETS ${targetName}
+		RUNTIME DESTINATION bin
+		LIBRARY DESTINATION lib
+		ARCHIVE DESTINATION lib
+	)		
+	
+	if(WIN32)
+		install(
+			FILES $<TARGET_PDB_FILE:${targetName}> 
+			DESTINATION bin 
+			OPTIONAL
+		)
+	else()
+		if(LINUX)
+			install(
+				FILES ${symbolsFile} 
+				DESTINATION lib
+				OPTIONAL)
+		else()
+			install(
+				FILES ${symbolsFile} 
+				DESTINATION bin
+				OPTIONAL)
+		endif()
+	endif()
+endfunction()
