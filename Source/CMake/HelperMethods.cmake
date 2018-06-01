eb197186317d88cbdb74274f63808a9a5689a134
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2018-06-01 02:09:51 -0400

    Fix clang pch issues

--- Source/CMake/HelperMethods.cmake
+++ Source/CMake/HelperMethods.cmake
@@ -407,3 +407,42 @@ function(copyBsfBinaries target srcDir)
 		endforeach()
 	endif()
 endfunction()
+
+# C too...
+function(find_clang_invalid_libc_pch_headers banned_files)
+	if (UNIX AND NOT APPLE)
+		set(LINUX True)
+	endif()
+
+	if (NOT LINUX)
+		return()
+	endif()
+
+	if ("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang")
+		execute_process(COMMAND ${CMAKE_C_COMPILER} -E -x c - -v
+						INPUT_FILE /dev/null
+						OUTPUT_FILE /dev/null
+			            ERROR_VARIABLE clang_c_raw_verbose)
+
+		string(REGEX MATCHALL "\n /[^\n]*" clang_c_search_dirs "${clang_c_raw_verbose}")
+		string(REPLACE "\n " "" clang_c_search_dirs "${clang_c_search_dirs}")
+
+		find_file(inttypes_c_location "inttypes.h" PATHS ${clang_c_search_dirs} PATH_SUFFIXES include NO_DEFAULT_PATH)
+
+		set(${banned_files} ${${banned_files}} ${inttypes_c_location} PARENT_SCOPE)
+	endif()
+
+	if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+		execute_process(COMMAND ${CMAKE_CXX_COMPILER} -E -x c++ - -v
+						INPUT_FILE /dev/null
+						OUTPUT_FILE /dev/null
+			            ERROR_VARIABLE clang_cxx_raw_verbose)
+
+		string(REGEX MATCHALL "\n /[^\n]*" clang_cxx_search_dirs "${clang_cxx_raw_verbose}")
+		string(REPLACE "\n " "" clang_cxx_search_dirs "${clang_cxx_search_dirs}")
+
+		find_file(inttypes_cxx_location "inttypes.h" PATHS ${clang_cxx_search_dirs} PATH_SUFFIXES include NO_DEFAULT_PATH)
+
+	endif()
+	set(${banned_files} ${inttypes_c_location} ${inttypes_cxx_location} PARENT_SCOPE)
+endfunction()
