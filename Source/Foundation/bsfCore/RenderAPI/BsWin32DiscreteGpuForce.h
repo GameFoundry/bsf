a5fb4294f00386b0029d8488c7c439887f6c3f9f
Patrick Rećko <guest_none@protonmail.com>
2018-12-02 22:41:22 +0100

    Feature: Add the Discrete GPU force header for use in executables.
    Used on Windows to force Optimus and Xpress to use Nvidia/AMD
    discrete GPU's instead of Intel integrated graphics.

---
+++ Source/Foundation/bsfCore/RenderAPI/BsWin32DiscreteGpuForce.h
@@ -0,0 +1,27 @@
+//************************************ bs::framework - Copyright 2018 Marko Pintera **************************************//
+//*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
+
+/**
+ * This Header file is used on Windows-based systems to force the operating system
+ * to use discrete graphics chips on laptops instead of the integrated graphics chips.
+ * This only affects Direct3D and OpenGL based RenderAPI back-ends.
+ * @note This header shouldn't be used on non-Windows based operating systems and
+ *        and it should only be included by final executables.
+ *        Incorrect usage will cause error during compilation.
+ */
+ 
+#if BS_PLATFORM == BS_PLATFORM_WIN32
+#if defined(BS_FORCE_DISCRETE_GPUS_INCLUDE_CHECK)
+	#error GPU force header file already included. Please, only include it in the program's executable. Aborting compilation.
+#else
+#define BS_FORCE_DISCRETE_GPUS_INCLUDE_CHECK
+ 
+extern "C"
+{
+__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
+#endif
+ 
+#endif 
