05d8a7052cd6283577bc4e155a7091a30c90142e
paolopaoletto <35465600+paolopaoletto@users.noreply.github.com>
2019-01-18 16:52:27 +0100

    Docs: Fix Copyright date.

--- Source/Foundation/bsfUtility/Utility/BsQuadtree.h
+++ Source/Foundation/bsfUtility/Utility/BsQuadtree.h
@@ -1,4 +1,4 @@
-//************************************ bs::framework - Copyright 2014 Marko Pintera **************************************//
+//************************************ bs::framework - Copyright 2018 Marko Pintera **************************************//
 //*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
 #pragma once
 
