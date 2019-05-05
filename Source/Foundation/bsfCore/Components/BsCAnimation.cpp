608bbdcbdb8e098f2b8a3719b7753ffc83844f8c
BearishSun <bearishsun@gmail.com>
2019-05-05 18:20:48 +0200

    Bugfix: Removing unnecessary debug output

--- Source/Foundation/bsfCore/Components/BsCAnimation.cpp
+++ Source/Foundation/bsfCore/Components/BsCAnimation.cpp
@@ -502,7 +502,6 @@ namespace bs
 				if (mAnimatedRenderable != nullptr)
 					bounds = mAnimatedRenderable->getBounds().getBox();
 
-				LOGWRN(toString(bounds.getCenter()) + " " + toString(bounds.getSize()));
 				mInternal->setBounds(bounds);
 			}
 		}
