1e117d26bffe151cb65602226b51805dbb9f1164
BearishSun <bearishsun@gmail.com>
2019-05-07 21:38:01 +0200

    Refactor: Custom threading primitives moved to 'bs' namespace

--- Source/Foundation/bsfUtility/Threading/BsThreading.h
+++ Source/Foundation/bsfUtility/Threading/BsThreading.h
@@ -10,44 +10,44 @@
 #include <condition_variable>
 #include "Threading/BsSpinLock.h"
 
-/** @addtogroup Threading
- *  @{
- */
-
-/** Returns the number of logical CPU cores. */
-#define BS_THREAD_HARDWARE_CONCURRENCY std::thread::hardware_concurrency()
-
-/** Returns the ThreadId of the current thread. */
-#define BS_THREAD_CURRENT_ID std::this_thread::get_id()
-
-/** Causes the current thread to sleep for the provided amount of milliseconds. */
-#define BS_THREAD_SLEEP(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-
-/** Wrapper for the C++ std::mutex. */
-using Mutex = std::mutex;
-
-/** Wrapper for the C++ std::recursive_mutex. */
-using RecursiveMutex = std::recursive_mutex;
-
-/** Wrapper for the C++ std::condition_variable. */
-using Signal = std::condition_variable;
-
-/** Wrapper for the C++ std::thread. */
-using Thread = std::thread;
-
-/** Wrapper for the C++ std::thread::id. */
-using ThreadId = std::thread::id;
-
-/** Wrapper for the C++ std::unique_lock<std::mutex>. */
-using Lock = std::unique_lock<Mutex>;
-
-/** Wrapper for the C++ std::unique_lock<std::recursive_mutex>. */
-using RecursiveLock = std::unique_lock<RecursiveMutex>;
-
-/** @} */
-
 namespace bs
 {
+	/** @addtogroup Threading
+	 *  @{
+	 */
+
+	 /** Returns the number of logical CPU cores. */
+#define BS_THREAD_HARDWARE_CONCURRENCY std::thread::hardware_concurrency()
+
+	/** Returns the ThreadId of the current thread. */
+#define BS_THREAD_CURRENT_ID std::this_thread::get_id()
+
+	/** Causes the current thread to sleep for the provided amount of milliseconds. */
+#define BS_THREAD_SLEEP(ms) std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+
+	/** Wrapper for the C++ std::mutex. */
+	using Mutex = std::mutex;
+
+	/** Wrapper for the C++ std::recursive_mutex. */
+	using RecursiveMutex = std::recursive_mutex;
+
+	/** Wrapper for the C++ std::condition_variable. */
+	using Signal = std::condition_variable;
+
+	/** Wrapper for the C++ std::thread. */
+	using Thread = std::thread;
+
+	/** Wrapper for the C++ std::thread::id. */
+	using ThreadId = std::thread::id;
+
+	/** Wrapper for the C++ std::unique_lock<std::mutex>. */
+	using Lock = std::unique_lock<Mutex>;
+
+	/** Wrapper for the C++ std::unique_lock<std::recursive_mutex>. */
+	using RecursiveLock = std::unique_lock<RecursiveMutex>;
+
+	/** @} */
+
 	/**
 	 * Policy that allows the calls its used in to pick between no locking and mutex locking through a template parameter.
 	 */
