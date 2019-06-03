a0bc0c021ce22e56d5741e7fc18e798d6167d2f5
paul gruenbacher <pgruenbacher@totalsim.us>
2019-06-03 17:29:11 -0400

    copy up

--- Source/Plugins/bsfEnTT/Scene/entActor.h
+++ Source/Plugins/bsfEnTT/Scene/entActor.h
@@ -6,9 +6,15 @@
 
 namespace ecs {
 
+// tag for disabling entities from being updated.
 struct Inactive {};
+/**
+ * Determines the mobility of the actor. This is used primarily as a performance hint to engine systems. Objects
+ * with more restricted mobility will result in higher performance. Any transform changes to immobile actors will
+ * be ignored. By default actor's mobility is unrestricted.
+ */
+struct TransformLocked {};
 
 void addChildToParent(Registry& reg, EntityType parent, EntityType child);
-
 
 } // namespace ecs
