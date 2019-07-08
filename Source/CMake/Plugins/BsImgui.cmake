c17577f55d9744ece2d7c691c2c4a06e27b97fe2
paul gruenbacher <pgruenbacher@totalsim.us>
2019-07-07 21:04:53 -0400

    fix advanced variables

--- Source/CMake/Plugins/BsImgui.cmake
+++ Source/CMake/Plugins/BsImgui.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 # will be configured as bsfImgui target. other targets can then set bsfImgui
 # as their dependency in their respective cmake files.
 
-set(FETCHCONTENT_QUIET 0)
+set(FETCHCONTENT_QUIET CACHE INTERNAL 0)
 
 FetchContent_Declare(
   imgui
@@ -14,16 +14,25 @@ FetchContent_Declare(
   GIT_PROGRESS TRUE
 )
 
-FetchContent_GetProperties(imgui)
-if(NOT imgui_POPULATED)
-	FetchContent_Populate(imgui)
+FetchContent_MakeAvailable(imgui)
 
-	add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR})
 
-	if (TARGET bsfImgui)
-		# Do something.
-		message( STATUS "bsfImgui target available")
-	else()		
-  		message( SEND_ERROR "bsfImgui target not available" )
-	endif()
+# mark as advanced so that FetchContent variables aren't being shown in the
+# cmake gui options, which we want to keep clean for user
+mark_as_advanced(
+	FETCHCONTENT_BASE_DIR
+	FETCHCONTENT_FULLY_DISCONNECTED
+	FETCHCONTENT_SOURCE_DIR_IMGUI
+	FETCHCONTENT_UPDATES_DISCONNECTED
+)
+
+if (TARGET bsfImgui)
+	# Do something.
+	message( STATUS "bsfImgui target available")
+else()		
+		message( SEND_ERROR "bsfImgui target not available" )
 endif()
+
+# mark as advanced so that FetchContent variables aren't being shown in the
+# cmake gui options, which we want to keep clean for user
+mark_as_advanced(imgui)
