aea541d1df1008b22ec6546750a47512c284c5ac
paul gruenbacher <pgruenbacher@totalsim.us>
2019-07-17 14:00:04 -0400

    update via comments

--- Source/CMake/Plugins/bsfImgui.cmake
+++ Source/CMake/Plugins/bsfImgui.cmake
@@ -1,11 +1,8 @@
 include(FetchContent)
 
-
-# if INCLUDE_IMGUI_PLUGIN is true, then git clone the imgui plugin (with
-# recursive submodules). it will be cloned into the build directory where it
-# will be configured as bsfImgui target. other targets can then set bsfImgui
-# as their dependency in their respective cmake files.
-
+# If BSF_PLUGIN_IMGUI is true, then git clone the imgui plugin (with recursive submodules). It will be cloned into
+# the build directory where it will be configured as bsfImgui target. Other targets can then set bsfImgui as their
+# dependency in their respective cmake files.
 set(FETCHCONTENT_QUIET CACHE INTERNAL 0)
 
 FetchContent_Declare(
@@ -23,9 +20,6 @@ if(NOT imgui_POPULATED)
   add_subdirectory(${imgui_SOURCE_DIR} ${imgui_BINARY_DIR})
 endif()
 
-
-# mark as advanced so that FetchContent variables aren't being shown in the
-# cmake gui options, which we want to keep clean for user
 mark_as_advanced(
 	FETCHCONTENT_BASE_DIR
 	FETCHCONTENT_FULLY_DISCONNECTED
@@ -35,11 +29,11 @@ mark_as_advanced(
 
 if (TARGET bsfImgui)
 	# Do something.
-	message( STATUS "bsfImgui target available")
+	message(STATUS "bsfImgui target available")
 else()		
-		message( SEND_ERROR "bsfImgui target not available" )
+	message(SEND_ERROR "bsfImgui target not available" )
 endif()
 
-# mark as advanced so that FetchContent variables aren't being shown in the
+# Mark as advanced so that FetchContent variables aren't being shown in the
 # cmake gui options, which we want to keep clean for user
 mark_as_advanced(imgui)
