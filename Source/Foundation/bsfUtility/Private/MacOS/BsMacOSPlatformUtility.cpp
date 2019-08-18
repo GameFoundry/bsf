4f954933365b1f5ceea392e59d2284fdc0f368c9
bearishsun <ad6c8384cb4728bf648f276ad63bb60d5443e58a>
2019-08-18 18:20:45 +0200

    Build: Fix for gcc/clang compilation broken with previous commits

--- Source/Foundation/bsfUtility/Private/MacOS/BsMacOSPlatformUtility.cpp
+++ Source/Foundation/bsfUtility/Private/MacOS/BsMacOSPlatformUtility.cpp
@@ -1,5 +1,6 @@
 //************************************ bs::framework - Copyright 2018 Marko Pintera **************************************//
 //*********** Licensed under the MIT license. See LICENSE.md for full terms. This notice is not to be removed. ***********//
+#include "Prerequisites/BsPrerequisitesUtil.h"
 #include "Utility/BsPlatformUtility.h"
 #include <uuid/uuid.h>
 #include <sys/sysctl.h>
